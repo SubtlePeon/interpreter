25ffb51c4ea6c4be75e7fbc56c5da98094124f2c
SubtlePeon <139384062+SubtlePeon@users.noreply.github.com>
2024-07-10 22:19:44 -0400

    Case conversion

--- src/token.rs
+++ src/token.rs
@@ -54,6 +54,41 @@ pub enum TokenType {
     Eof,
 }
 
+// CodeCrafters needs the names of the token types to be in
+// `SCREAMING_SNAKE_CASE`, so this is the function to convert their
+// names.
+impl std::fmt::Display for TokenType {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let name = match self {
+            Self::LeftParen    => "LEFT_PAREN",
+            Self::RightParen   => "RIGHT_PAREN",
+            Self::LeftBrace    => "LEFT_BRACE",
+            Self::RightBrace   => "RIGHT_BRACE",
+            Self::Comma        => "COMMA",
+            Self::Dot          => "DOT",
+            Self::Minus        => "MINUS",
+            Self::Plus         => "PLUS",
+            Self::Semicolon    => "SEMICOLON",
+            Self::Slash        => "SLASH",
+            Self::Star         => "STAR",
+            Self::Bang         => "BANG",
+            Self::BangEqual    => "BANG_EQUAL",
+            Self::Equal        => "EQUAL",
+            Self::EqualEqual   => "EQUAL_EQUAL",
+            Self::Greater      => "GREATER",
+            Self::GreaterEqual => "GREATER_EQUAL",
+            Self::Less         => "LESS",
+            Self::LessEqual    => "LESS_EQUAL",
+            Self::Ident        => "IDENT",
+            Self::Number       => "NUMBER",
+            Self::String       => "STRING",
+            Self::Eof          => "EOF",
+        };
+
+        write!(f, "{}", name)
+    }
+}
+
 #[derive(Clone, Debug)]
 pub struct Token<'a> {
     ty: TokenType,
@@ -73,5 +108,11 @@ impl<'a> Token<'a> {
 
     pub fn source(&'a self) -> &'a str {
         self.source
+    }
+}
+
+impl<'a> std::fmt::Display for Token<'a> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{} {} {}", self.ty, self.source, "null")
     }
 }
